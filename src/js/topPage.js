c9fc798a4d1e3d709969164fa9654518db4a589f
ykurasawa <ykurajpx@gmail.com>
2026-01-28 00:36:25 +0000

    Initial commit with Dockerfile and App

---
+++ src/js/topPage.js
@@ -0,0 +1,8 @@
+function execute() {
+  var elem = document.getElementById("output");
+  var now = new Date();
+  var hour = now.getHours();
+  var min = now.getMinutes();
+  elem.innerHTML = "ただいまの時刻は" + hour + ":" + min + "です";
+}
+
